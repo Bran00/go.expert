457e4e1007b258a75c7c2ffd67b1b4d6613e8d66
Brando Rocha <brandorocha00@gmail.com>
2025-04-06 16:32:05 -0300

    feat: class f010

---
+++ 2/main.go
@@ -0,0 +1,16 @@
+package main
+
+const a = "Hello"
+
+var (
+    b bool = true
+    c int = 42
+    d string = "World"
+    e float64 = 3.14
+)
+
+func main() {
+    a := "X"
+    a = "fd"
+    println(a)
+}

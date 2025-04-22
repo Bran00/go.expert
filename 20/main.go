16fce16da1e50b2df815c3afe053d014bd75b071
Brando Rocha <brandorocha00@gmail.com>
2025-04-22 19:56:07 -0300

    feat: working with generics/2

--- 20/main.go
+++ 20/main.go
@@ -1,6 +1,12 @@
 package main
 
-func Soma[T int | float64](m map[string]T) T {
+type MyNumber int
+
+type Number interface {
+	~int | float64
+}
+
+func Soma[T Number](m map[string]T) T {
 	var soma T
 	for _, v := range m {
 		soma += v
@@ -8,9 +14,18 @@ func Soma[T int | float64](m map[string]T) T {
 	return soma
 }
 
+func Compara[T comparable] (a T, b T) bool {
+	if a == b {
+		return true
+	}
+	return false
+}
+
 func main() {
 	m := map[string]int{"Brando": 1000, "Luca": 2000, "Gustavo": 3000}
 	m2 := map[string]float64{"Brando": 1000.65, "Luca": 2000.32, "Gustavo": 3000.98}
 	println(Soma(m))
 	println(Soma(m2))
+
+	println(Compara(10, 10.4))
 }

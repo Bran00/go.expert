0a2efe63be8dfe03dea935e7dd374ad9cfbf0030
Brando Rocha <brandorocha00@gmail.com>
2025-04-24 20:29:33 -0300

    feat: working with installing packages

--- 21/go.mod
+++ 21/go.mod
@@ -1,3 +1,7 @@
 module github.com/Bran00/go.expert
 
 go 1.23.4
+
+//go mod tidy
+
+require github.com/google/uuid v1.6.0
--- 21/main.go
+++ 21/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/Bran00/go.expert/matematica"
+	"github.com/google/uuid"
 )
 
 func main() {
@@ -12,4 +13,5 @@ func main() {
 	fmt.Println("Resultado: ", s)
 	fmt.Println((matematica.A))
 	fmt.Println(carro)
+	fmt.Println(uuid.New())
 }

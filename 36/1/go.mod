a3ad1603a02c4035377c257eac60873dc2ce92d3
Brando Rocha <brandorocha00@gmail.com>
2025-05-03 12:48:26 -0300

    feat: insering data in database

--- 36/1/go.mod
+++ 36/1/go.mod
@@ -2,4 +2,9 @@ module github.com/Bran00/go.expert/36/1
 
 go 1.23.4
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/go-sql-driver/mysql v1.9.2
+	github.com/google/uuid v1.6.0
+)
+
+require filippo.io/edwards25519 v1.1.0 // indirect
--- 36/1/main.go
+++ 36/1/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/google/uuid"
 )
 
@@ -19,5 +23,30 @@ func NewProduct(name string, price float64) *Product {
 }
 
 func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
+	product := NewProduct("Ideapad", 1899.0)
+	err = insertProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Product inserted successfully")
+}
+
+func insertProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("INSERT INTO products (id, name, price) VALUES (?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		return err
+	}
+	return nil
 }

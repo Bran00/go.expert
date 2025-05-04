64c969284fd3dedd95779f3d51d3c8cdd35ae436
Brando Rocha <brandorocha00@gmail.com>
2025-05-04 15:00:18 -0300

    feat: config and creating operations using GORM

---
+++ 36/2/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	ID	 		int 		`gorm:"primaryKey"`
+	Name	 	string
+	Price	 	float64
+}
+
+func main() {
+	dsn := "root:root@tcp(localhost:3306)/goexpert"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&Product{})
+
+	// create
+	/* db.Create(&Product{
+		Name: "Alienware",
+		Price: 2500.00,
+	}) */
+
+	// create batch
+	products := []Product{
+		{Name: "IdeaPad", Price: 1500.00},
+		{Name: "MacBook", Price: 3000.00},
+		{Name: "Surface", Price: 2000.00},
+	}
+	db.Create(&products)
+}

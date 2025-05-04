1499d28b5e9f51e5c6841173065a9dc7d96373d3
Brando Rocha <brandorocha00@gmail.com>
2025-05-04 15:42:55 -0300

    feat: realizing soft delete

--- 36/2/main.go
+++ 36/2/main.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 type Product struct {
-	ID	 		int 		`gorm:"primaryKey"`
-	Name	 	string
-	Price	 	float64
+	ID    int `gorm:"primaryKey"`
+	Name  string
+	Price float64
+	gorm.Model
 }
 
 func main() {
-	dsn := "root:root@tcp(localhost:3306)/goexpert"
+	dsn := "root:root@tcp(localhost:3306)/goexpert?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
@@ -74,13 +75,12 @@ func main() {
 	/* var p Product
 	db.First(&p, 1)
 	p.Name = "Alienware X17"
-	db.Save(&p)
-
-	var p2 Product
-	db.First(&p2, 1)
-	fmt.Println(p2.Name)
-
-	db.Delete(&p2) */
-
+	db.Save(&p) 
 	
+		var p2 Product
+		db.First(&p2, 1)
+		//fmt.Println(p2.Name)
+
+		db.Delete(&p2)
+ */
 }

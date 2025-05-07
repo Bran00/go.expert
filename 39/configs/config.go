9a70b49d0968dcd1abb469c5f91f2c61f6d3fdee
Brando Rocha <brandorocha00@gmail.com>
2025-05-07 20:06:37 -0300

    feat: initializing studies with api using configs

---
+++ 39/configs/config.go
@@ -0,0 +1,20 @@
+package configs
+
+var cfg *conf
+
+type conf struct {
+	DBDriver      string
+	DBHost        string
+	DBPort        string
+	DBUser        string
+	DBPassword    string
+	DBName        string
+	WebServerPort string
+	JWTSecret			string
+	JWTExpiresIN	int
+}
+
+func LoadConfig(path string) (*conf, error){
+
+}
+

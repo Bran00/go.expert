5fc796ad3e21acccc2a12031bd07c7701fc5da10
Brando Rocha <brandorocha00@gmail.com>
2025-05-11 13:48:55 -0300

    feat: docs to create user

--- 39/internal/infra/webserver/handlers/user_handlers.go
+++ 39/internal/infra/webserver/handlers/user_handlers.go
@@ -12,6 +12,10 @@ import (
 	"github.com/go-chi/jwtauth"
 )
 
+type Error struct {
+	Message string `json:"message"`
+}
+
 type UserHandler struct {
 	UserDB       database.UserInterface
 }
@@ -22,6 +26,16 @@ func NewUserHandler(userDB database.UserInterface) *UserHandler {
 	}
 }
 
+// Create user godoc
+// @Summary      Create user
+// @Description  Create user
+// @Tags         user
+// @Accept       json
+// @Produce      json
+// @Param        user  body     dto.CreateUserInput  true  "user request"
+// @Success      201
+// @Failure      500  {object}  Error
+// @Router       /users [post]
 func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
 	jwt := r.Context().Value("jwt").(*jwtauth.JWTAuth)
 	jwtExpiresIn := r.Context().Value("jwt_expires_in").(int)
@@ -61,6 +75,16 @@ func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(acessToken)
 }
 
+// Create user godoc
+// @Summary      Create user
+// @Description  Create user
+// @Tags         user
+// @Accept       json
+// @Produce      json
+// @Param        request  body   dto.CreateUserInput  true  "user request"
+// @Success      201
+// @Failure			 500  {object}  Error
+// @Router			 /users [post]
 func (h *UserHandler) Create(w http.ResponseWriter, r *http.Request) {
 	var user dto.CreateUserInput
 	err := json.NewDecoder(r.Body).Decode(&user)
@@ -72,12 +96,18 @@ func (h *UserHandler) Create(w http.ResponseWriter, r *http.Request) {
 	u, err := entity.NewUser(user.Name, user.Email, user.Password)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
 		return
+
+
 	}
 
 	err = h.UserDB.Create(u)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
 		return
 	}
 	w.WriteHeader(http.StatusCreated)

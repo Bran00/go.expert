c79a35f46cbb61f28be2cb61ea449d6019ffa8e7
Brando Rocha <brandorocha00@gmail.com>
2025-05-17 14:59:27 -0300

    feat: creating resolver to category

--- 44/graph/resolver.go
+++ 44/graph/resolver.go
@@ -1,7 +1,11 @@
 package graph
 
+import "github.com/Bran00/go.expert/44/internal/database"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct{
+	CategoryDB *database.Category
+}

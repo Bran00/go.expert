1725a04d7f6b2012b656279d4ddedf5027f1eda5
Brando Rocha <brandorocha00@gmail.com>
2025-05-17 15:35:44 -0300

    feat: making queries to category

--- 44/graph/schema.resolvers.go
+++ 44/graph/schema.resolvers.go
@@ -31,7 +31,19 @@ func (r *mutationResolver) CreateCourse(ctx context.Context, input model.NewCour
 
 // Categories is the resolver for the categories field.
 func (r *queryResolver) Categories(ctx context.Context) ([]*model.Category, error) {
-	panic(fmt.Errorf("not implemented: Categories - categories"))
+	categories, err := r.CategoryDB.FindAll()
+	if err != nil {
+		return nil, err
+	}
+	result := make([]*model.Category, len(categories))
+	for i, category := range categories {
+		result[i] = &model.Category{
+			ID:          category.ID,
+			Name:        category.Name,
+			Description: &category.Description,
+		}
+	}
+	return result, nil
 }
 
 // Courses is the resolver for the courses field.

c8dd6c35010db568a3085dde845fbf45c5c0fa32
Brando Rocha <brandorocha00@gmail.com>
2025-05-24 10:14:58 -0300

    feat: developing upload function

--- 46/cmd/uploader/main.go
+++ 46/cmd/uploader/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"google.golang.org/genproto/googleapis/iam/credentials/v1"
-	"google.golang.org/grpc/experimental/credentials"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 )
 
 var (
@@ -14,14 +16,16 @@ var (
 )
 
 func init() {
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("us-east-1"),
-		Credentials: credentials.NewStaticCredentials(
-			"your-access-key-id",
-			"your-secret",
-			"",
-		),
-	})
+	sess, err := session.NewSession(
+		&aws.Config{
+			Region: aws.String("us-east-1"),
+			Credentials: credentials.NewStaticCredentials(
+				"your-access-key-id",
+				"your-secret",
+				"",
+			),
+		},
+	)
 	if err != nil {
 		panic(err)
 	}
@@ -30,5 +34,30 @@ func init() {
 }
 
 func main() {
+	dir, err := os.Open("./tmp")
+	if err != nil {
+		panic(err)
+	}
+	defer dir.Close()
+}
 
+func uploadFile(fileName string) {
+	completeFileName := fmt.Sprintf("./tmp/%s", fileName)
+	fmt.Printf("Uploading file %s to bucket %s\n", completeFileName, s3Bucket)
+	f, err := os.Open(completeFileName)
+	if err != nil {
+		fmt.Printf("Error opening file %s: %v\n", completeFileName, err)
+		return
+	}
+	defer f.Close()
+	_, err = s3Client.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(s3Bucket),
+		Key:    aws.String(fileName),
+		Body:   f,
+	})
+	if err != nil {
+		fmt.Printf("Error uploading file %s: %v\n", fileName, err)
+		return
+	}
+	fmt.Printf("Successfully uploaded %s to %s\n", fileName, s3Bucket);
 }

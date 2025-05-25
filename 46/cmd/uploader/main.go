e75f01a35923bd831ac4c4a9fe12d4ec3777bcfb
Brando Rocha <brandorocha00@gmail.com>
2025-05-25 14:57:22 -0300

    feat: limit to upload

--- 46/cmd/uploader/main.go
+++ 46/cmd/uploader/main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"sync"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/aws/credentials"
 )
 
 var (
 	s3Client *s3.S3
 	s3Bucket string
+	wg		sync.WaitGroup
 )
 
 func init() {
@@ -20,8 +23,8 @@ func init() {
 		&aws.Config{
 			Region: aws.String("us-east-1"),
 			Credentials: credentials.NewStaticCredentials(
-			os.Getenv("AWS_ACCESS_KEY_ID"),
-			os.Getenv("AWS_SECRET_ACCESS_KEY"),
+				os.Getenv("AWS_ACCESS_KEY_ID"),
+				os.Getenv("AWS_SECRET_ACCESS_KEY"),
 				"",
 			),
 		},
@@ -34,30 +37,36 @@ func init() {
 }
 
 func main() {
-	dir, err := os.Open("./tmp")
+	dir, err := os.Open("../../tmp")
 	if err != nil {
 		panic(err)
 	}
 	defer dir.Close()
+	uploadControl := make(chan struct{}, 100)
 	for {
-		files, err := dir.Readdirnames(1)
+		files, err := dir.ReadDir(1)
 		if err != nil {
-			if err.Error() == "EOF" {
+			if err == io.EOF {
 				break
 			}
 			fmt.Printf("Error reading directory: %v\n", err)
 			continue
 		}
-		uploadFile(files[0].Name())
+		wg.Add(1)
+		uploadControl <- struct{}{}
+		go uploadFile(files[0].Name(), uploadControl)
 	}
+	wg.Wait()
 }
 
-func uploadFile(fileName string) {
-	completeFileName := fmt.Sprintf("./tmp/%s", fileName)
+func uploadFile(fileName string, uploadControl <-chan struct{}) {
+	defer wg.Done()
+	completeFileName := fmt.Sprintf("../../tmp/%s", fileName)
 	fmt.Printf("Uploading file %s to bucket %s\n", completeFileName, s3Bucket)
 	f, err := os.Open(completeFileName)
 	if err != nil {
 		fmt.Printf("Error opening file %s: %v\n", completeFileName, err)
+		<-uploadControl
 		return
 	}
 	defer f.Close()
@@ -68,7 +77,9 @@ func uploadFile(fileName string) {
 	})
 	if err != nil {
 		fmt.Printf("Error uploading file %s: %v\n", fileName, err)
+		<-uploadControl
 		return
 	}
-	fmt.Printf("Successfully uploaded %s to %s\n", fileName, s3Bucket);
+	fmt.Printf("Successfully uploaded %s to %s\n", fileName, s3Bucket)
+	<-uploadControl
 }

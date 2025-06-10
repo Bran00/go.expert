d525e0365261836582f073611e840e2766fb9738
Brando Rocha <brandorocha00@gmail.com>
2025-06-10 20:34:40 -0300

    feat: creating a new case

---
+++ 49/internal/usecase/add_course_uow.go
@@ -0,0 +1,45 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/Bran00/go.expert/49/pkg/uow"
+)
+
+type AddCourseUseCaseUow struct {
+	Uow uow.UowInterface
+}
+
+func NewAddCourseUseCaseUow(uow uow.UowInterface) *AddCourseUseCaseUow {
+	return &AddCourseUseCaseUow{
+		Uow: uow,
+	}
+}
+
+func (a *AddCourseUseCaseUow) Execute(ctx context.Context, input InputUseCase) error {
+	return a.Uow.Do(ctx, (func(uow *uow.Uow) error{
+		// tudo que colocarmos aqui, será feito um begin e um commit
+		return nil
+	}))
+
+	/* category := entity.Category{
+		Name: input.CategoryName,
+	}
+
+	err := a.CategoryRepository.Insert(ctx, category)
+	if err != nil {
+		return err
+	}
+
+	course := entity.Course{
+		Name:       input.CourseName,
+		CategoryID: input.CourseCategoryID,
+	}
+
+	err = a.CourseRepository.Insert(ctx, course)
+	if err != nil {
+		return err
+	}
+
+	return nil */
+}
